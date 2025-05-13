47ee547116d8e7fa4b2266824cc774bf565046e5
Yash <67926590+Yash094@users.noreply.github.com>
2025-05-13 17:44:45 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,42 @@
 
 import {
   ConnectButton,
+  TransactionButton,
 } from "thirdweb/react";
 import { client } from "./client";
-import { defineChain,  } from "thirdweb";
+import { avalancheFuji } from "thirdweb/chains";
+import { getContract, prepareContractCall, toWei } from "thirdweb";
+import { inAppWallet } from "thirdweb/wallets";
 
 
-// Main page component
+
 export default function Home() {
-
+  const contract = getContract({
+    client,
+    chain: avalancheFuji,
+    address: "0xfa723da21975aB2EABf6F2f560Fa18f8fa97401E",
+  })
+  const wallet = inAppWallet({
+    auth: {
+      mode: "redirect",
+      options: ["google", "apple", "facebook", "discord", "line", "x", "coinbase", "farcaster", "telegram", "github", "twitch", "steam", "guest", "backend", "email", "phone", "passkey", "wallet"]
+    },
+  });
 
   return (
     <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
       <div className="py-20">
         <div className="flex flex-col items-center gap-6">
           <div className="flex gap-4">
-            {/* Connect wallet button */}
             <ConnectButton
               client={client}
-              accountAbstraction={{
-                chain: defineChain(56),
-                sponsorGas: true,
-              }}
+              wallets={[wallet]}
+            
             />
 
           </div>
+
+
         </div>
       </div>
     </main>
